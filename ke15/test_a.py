2ccadd23d15c5f39f0ab877dd2313e852b9c1d26
fzx <18265822856@163.com>
2022-08-06 14:13:30 +0800

    setup_module、setup_class、setup执行范围

--- ke15/test_a.py
+++ ke15/test_a.py
@@ -1,42 +1,40 @@
 def setup_module():
-    print("整个py文件只执行一次")
-
+    print("setup_module  整个py文件只执行一次")
 
 
 class TestDemo(object):
 
     def setup_class(self):
-        print("整个class 用例 之前，只执行一次")
+        print("Demo1 setup_class  整个class 用例 之前，只执行一次")
 
     def setup(self):
-        print("每个用例都会执行")
+        print("Demo1 setup  Demo1每个用例都会执行")
 
     def test_1(self):
         """用例1"""
-        print("case 11111111")
+        print("case 111")
         assert 1 == 1
 
     def test_2(self):
         """用例1"""
-        print("case 11111111")
+        print("case 222")
         assert 1 == 1
 
 
 class TestDemo2(object):
 
     def setup_class(self):
-        print("整个class 用例 之前，只执行一次")
+        print("Demo2 setup_class  整个class 用例 之前，只执行一次")
 
     def setup(self):
-        print("每个用例都会执行")
+        print("Demo2 setup 每个用例都会执行")
 
     def test_3(self):
         """用例1"""
-        print("case 11111111")
+        print("case 333")
         assert 1 == 1
 
     def test_4(self):
         """用例1"""
-        print("case 11111111")
+        print("case 444")
         assert 1 == 1
-
