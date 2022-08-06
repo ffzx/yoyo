a992abc3c76650af1c826393b270d3bfeb6b5797
fzx <18265822856@163.com>
2022-08-06 12:35:57 +0800

    ke13

--- ke13/张三.py
+++ ke13/张三.py
@@ -2,6 +2,9 @@
 标准写法
 声明 x传什么类型
 声明返回的结果是什么对象
+
+ -> 声明类型，方便编辑器能识别 返回的结果继续调用方法
+
 """
 from typing import List
 
@@ -19,5 +22,4 @@ def func(x:str, y:str) -> List:
 if __name__ == '__main__':
     # 仅仅只在当前py 文件执行
     r = func("hello", "world")  # 自测的内容
-    # -> 声明的结果，方便编辑器能识别 返回的结果继续调用方法
     print(r)
