337f7c0a8b0aa6662e10f45811c6215441b596c7
fzx <18265822856@163.com>
2022-08-06 11:12:13 +0800

    json串和dict数据格式

--- ke11/f.py
+++ ke11/f.py
@@ -2,19 +2,19 @@ import json
 
 # json 不是基础的数据类型    str list tuple dict
 
-a = {
-    'name': "yoyo",
-    "mail": "123@qq.com",
-    "age": "22",
-    "tel": "12345678901"
-}
-print(a)  # {'name': 'yoyo', 'mail': '123@qq.com', 'age': '22', 'tel': '12345678901'}
-print(type(a))
-b = json.dumps(a)  # {"name": "yoyo", "mail": "123@qq.com", "age": "22", "tel": "12345678901"}
-print(b)
-print(type(b))  # json就是(有规律的)字符串
+# a = {
+#     'name': "yoyo",
+#     "mail": "123@qq.com",
+#     "age": "22",
+#     "tel": "12345678901"
+# }
+# print(a)  # {'name': 'yoyo', 'mail': '123@qq.com', 'age': '22', 'tel': '12345678901'}
+# print(type(a))
+# b = json.dumps(a)  # {"name": "yoyo", "mail": "123@qq.com", "age": "22", "tel": "12345678901"}
+# print(b)
+# print(type(b))  # json就是(有规律的)字符串
 
-# json的规律
+
 """
 1.json对象 严格的双引号  {"key": "value", "key11": "value2"}
 2.json数组  ["a", "b1", 1, true, null, {"key": "value"}]
@@ -24,7 +24,7 @@ print(type(b))  # json就是(有规律的)字符串
 7.null
 """
 
-aa = ["a", "b1", 1, True, None, {"key": "value"}]
+aa = ['a', 'b1', 1, True, None, {"key": "value"}]
 # 转json
 bb = json.dumps(aa)
 print(bb)  # ["a", "b1", 1, true, null, {"key": "value"}]
