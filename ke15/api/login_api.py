20180e874b46653d209faa3535c9e88a3070471a
fzx <18265822856@163.com>
2022-08-06 14:01:14 +0800

    api和testcase封装

--- ke15/api/login_api.py
+++ ke15/api/login_api.py
@@ -2,7 +2,11 @@ import requests
 from requests import Response, Session
 
 
-def login(s:Session, username="test11", password="123456") -> Response:
+"""
+先写api,传缺省参数
+写testcase的时候，调佣api，传其他需要测试的参数
+"""
+def login(s: Session, username="test11", password="123456") -> Response:
     url = "http://49.235.92.12:7005/api/v1/login"
     body = {
         "username": username,
